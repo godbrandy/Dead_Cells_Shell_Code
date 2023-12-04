72533409cbbca4674350f99b5968e23473c7403e
godbrandy <125593220+godbrandy@users.noreply.github.com>
2023-12-04 15:46:33 +0100

    added custom deallocator for VirtualAllocEx

--- Dead_Cells_External_Shell_Code_Injection/cheat.h
+++ Dead_Cells_External_Shell_Code_Injection/cheat.h
@@ -14,12 +14,34 @@ public:
 	bool IsBufferEmpty() { return buffer.empty(); };
 
 private:
+	class VirtualDeallocate
+	{
+	public:
+		VirtualDeallocate(HANDLE proc_handle)
+			:
+			proc_handle{ proc_handle }
+		{}
+		VirtualDeallocate() = default;
+		void operator()(void* ptr) const
+		{
+			if (ptr)
+			{
+				VirtualFreeEx(proc_handle, ptr, 0, MEM_RELEASE);
+			}
+		}
+	private:
+		HANDLE proc_handle;
+	};
+
+private:
 	BYTE* hook_addr{ nullptr };
 	bool og_bytes_saved{ false };
-	uintptr_t alloc_addr{ 0 };
+	//uintptr_t alloc_addr{ 0 };
+	std::unique_ptr<uintptr_t, VirtualDeallocate> alloc_addr;
 	size_t len;
 	std::vector<BYTE> buffer;
 	std::vector<BYTE> shell_code;
 	Game& game;
 	Pattern& pattern;
+
 };
